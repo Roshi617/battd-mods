67291979bae86547149b59d7b10dc3b4440935cf
unknown <x.wilhoyt@gmail.com>
2026-06-21 20:06:17 -0500

    Add code for savegame password generator (no release at this time)

---
+++ AppIDPasswordTest/AppIDPasswordTest/Core.cs
@@ -0,0 +1,37 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2CppNinjakiwi.Players.Files;
+
+[assembly: MelonInfo(typeof(AppIDPasswordTest.Core), "AppIDPasswordTest", "1.0.0", "xenia617", null)]
+[assembly: MelonGame("Ninja Kiwi", "Bloons Adventure Time TD")]
+
+namespace AppIDPasswordTest
+{
+    public class Core : MelonMod
+    {
+        public override void OnUpdate()
+        {
+
+        }
+
+        [HarmonyPatch(typeof(PasswordGenerator), nameof(PasswordGenerator.GetPassword))]
+        public class PasswordPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PasswordGenerator __instance)
+            {
+                //MelonLogger.Msg("idk patched maybe");
+
+                StringAndVersion strandver = __instance.GetLatestPassword();
+                string value = strandver.Value;
+                string version = strandver.Version.ToString();
+                MelonLogger.Msg("Value: " + value);
+                MelonLogger.Msg("Version: " + version);
+
+                string password = __instance.GetPassword(strandver.Version);
+                MelonLogger.Msg(password);
+                
+            }
+        }
+    }
+}
