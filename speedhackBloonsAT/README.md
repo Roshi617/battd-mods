24f76e4c55b3555ced8b4bebb368191fb1016160
unknown <x.wilhoyt@gmail.com>
2026-08-11 01:25:15 -0500

    Add speedhack mod inital release

---
+++ speedhackBloonsAT/README.md
@@ -0,0 +1,8 @@
+Speedhack mod for BATTD.
+
+F2 key: 2x speed
+F3 key: 5x speed
+F1 key: Normal fast forward speed
+F8 key: debug key used for testing (prints values to console)
+
+Put .dll in Mods folder. Melonloader 0.7.1 needed.
---
+++ speedhackBloonsAT/speedhackBloonsAT/Core.cs
@@ -0,0 +1,58 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Il2CppAssets.Scripts;
+
+[assembly: MelonInfo(typeof(speedhackBloonsAT.Core), "speedhackBloonsAT", "1.0.0", "xenia617", null)]
+[assembly: MelonGame("Ninja Kiwi", "Bloons Adventure Time TD")]
+
+namespace speedhackBloonsAT
+{
+    public class Core : MelonMod
+    {
+        public static float speed = 0.02f;
+        public static float fasterSpeed = 0.033333336f;
+        public static float evenFasterSpeed = 0.08333334f;
+        public static float normalAmount = 0.016666668f;
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized SpeedhackBATTD.");
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            //debug key (for testing and checking values)
+            if (Input.GetKeyDown(KeyCode.F8))
+            {
+                MelonLogger.Msg("FFTimeScale: " + Constants.fastFowardTimeScale);
+                MelonLogger.Msg("RegTimeScale: " + Constants.regularTimeScale);
+                MelonLogger.Msg("fixedUpdateTime: " + Constants.fixedUpdateTime);
+            }
+
+            // 2x speed key
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                speed = fasterSpeed;
+                Constants.fastFowardTimeScale = speed;
+                MelonLogger.Msg("Speed set to 2x faster");
+            }
+
+            // 5x speed key
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                speed = evenFasterSpeed;
+                Constants.fastFowardTimeScale = speed;
+                MelonLogger.Msg("Speed set to 5x faster");
+            }
+
+            // Normal FF speed key
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                speed = normalAmount;
+                Constants.fastFowardTimeScale = speed;
+                MelonLogger.Msg("Speed set to normal speed.");
+            }
+        }
+    }
+}
